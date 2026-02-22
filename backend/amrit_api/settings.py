a9c5286602b6559643fb7766cf320a7cd39d44b5
Amogh-1416 <samudralaamogh16@gmail.com>
2026-02-22 17:32:04 +0530

    feat: create spatial models for drones, survivor clusters, and damage reports

--- backend/amrit_api/settings.py
+++ backend/amrit_api/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.gis",
     "channels",
+    'core',
 ]
 
 MIDDLEWARE = [
---
+++ backend/core/models.py
@@ -0,0 +1,58 @@
+from django.contrib.gis.db import models
+
+# Create your models here.
+class Drone(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    battery_level = models.IntegerField(default=100)
+
+    location = models.PointField(srid=4326, null=True, blank=True)
+
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class SurvivorCluster(models.Model):
+    location = models.PointField(srid=4326)
+
+    estimated_count = models.IntegerField(default=1)
+
+    radius_meters = models.FloatField(default=0.0)
+
+    confidece_score = models.FloatField()
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    is_rescued = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Cluster of {self.estimated_count} survivors - Confidence: {self.confidece_score}%"
+    
+
+class DamageReport(models.Model):
+    DAMAGE_TYPES = [
+        ('FIRE', 'Fire'),
+        ('FLOOD', 'Flood'),
+        ('COLLAPSE', 'Collapsed Structure'),
+        ('ROAD_BLOCK', 'Blocked Road'),
+        ('OTHER', 'Other'),
+    ]
+
+    SEVERITY_LEVELS = [
+        ('LOW', 'Low'),
+        ('MODERATE', 'Moderate'),
+        ('HIGH', 'High'),
+        ('CRITICAL', 'Critical'),
+    ]
+
+    location = models.PointField(srid=4326)
+
+    damage_type = models.CharField(max_length=20, choices=DAMAGE_TYPES)
+    severity_level = models.CharField(max_length=15, choices=SEVERITY_LEVELS)
+
+    description = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "f{self.get_damage_type_display()} ({self.get_severity_level_display()})"
